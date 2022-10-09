d0c9d2badfa9b30ac8e8fe16d44135d5ca3f6ee6
PAI Star <88820147+QingshanTown@users.noreply.github.com>
2022-10-09 15:26:41 +0800

    Initial commit

---
+++ Assets/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    float carspeed = 0;
+    Vector2 startPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.startPos = Input.mousePosition;
+            //this.carspeed = 0.1f;
+        }else if(Input.GetMouseButtonUp(0)){
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+            this.carspeed = swipeLength / 500f;
+            this.GetComponent<AudioSource>().Play();
+        }
+        transform.Translate(this.carspeed, 0, 0);
+        this.carspeed *= 0.98f;
+	}
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+	// Use this for initialization
+	void Start () {
+        this.car = GameObject.Find("car");//按 name 查找 GameObject，然后返回它。
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (length > 0)
+        {
+            this.distance.GetComponent<Text>().text = "距离目标" + length.ToString("F2") + "m";
+        }
+        else
+        {
+            this.car.transform.position = this.flag.transform.position;
+            this.distance.GetComponent<Text>().text = "游戏结束";
+        }
+
+    }
+}
